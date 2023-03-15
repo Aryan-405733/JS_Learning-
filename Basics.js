cb73cfcdca6ada3d4ad73e549bf0bcd0a740919a
Aryan Sharma <as405733@gmail.com>
2023-03-15 17:07:25 +0530

    second commit

--- Basics.js
+++ Basics.js
@@ -2,9 +2,9 @@ let nam = "Aryan"; // string
 let age = 23; // Number
 let height = 1.89; // Number
 
-// console.log(nam);
-// console.log(age);
-// console.log(height);
+console.log(nam);
+console.log(age);
+console.log(height);
 
 age = 25;
 console.log(age);
